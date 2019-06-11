5f749f277a916446c937580c8e5140efc1f3741d
Ajay Siwach <ajaysiwach16@gmail.com>
2019-06-11 10:56:58 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    AUTH_ENDPOINT: '',
+    AUTH_CLIENT_ID: '',
+    AUTH_CLIENT_SECRET: '',
+    AUTH_RESPONSE_TYPE: 'token',
+    AUTH_SCOPE: '',
+    AUTH_LOGIN_URI: '<your-auth-action>',
+    AUTH_REDIRECT_URI: '<your-app-homepage>'
+  }
